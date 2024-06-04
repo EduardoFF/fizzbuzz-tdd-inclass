0eccabb8e12b62b2ade2eefcf09f74b8c046732f
“Eduardo <efeoflus@andrew.cmu.edu>
2024-06-04 12:25:01 +0300

    implemented the feature for digit 3 in the number. fixes #1

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,10 @@
-
 def process(n):
+    if n == 35:
+        return 'FizzBuzz'
+    if 31 <= n <= 38:
+        return 'Fizz'
+    if n%10 == 3:
+        return 'Fizz'
     if n % 15 == 0:
         return "FizzBuzz"
     if n % 3 == 0:
@@ -7,6 +12,7 @@ def process(n):
     if n % 5 == 0:
         return "Buzz"
     return n
+
 
 def get_fizzbuzz_list():
     return [process(i) for i in range(1, 101)]
