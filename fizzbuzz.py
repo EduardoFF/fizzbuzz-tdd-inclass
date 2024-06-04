8534d8f1b4098a2ca9442ee50ee37985626a6013
“Eduardo <efeoflus@andrew.cmu.edu>
2024-06-04 12:18:09 +0300

    ready to deploy

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,17 @@
 
 def process(n):
-    return 1
+    if n % 15 == 0:
+        return "FizzBuzz"
+    if n % 3 == 0:
+        return "Fizz"
+    if n % 5 == 0:
+        return "Buzz"
+    return n
 
-for number in range(1,101):
-    print(process(number))
+def get_fizzbuzz_list():
+    return [process(i) for i in range(1, 101)]
+
+def fizzbuzz():
+    L = get_fizzbuzz_list()
+    for number in range(1, 101):
+        print(L[number])
--- test_simple.py
+++ test_simple.py
@@ -1,4 +1,32 @@
-from fizzbuzz import process
+from fizzbuzz import process, get_fizzbuzz_list
 
 def test_number_one():
     assert process(1) == 1
+
+def test_number_simple_numbers():
+    assert process(2) == 2
+    assert process(44) == 44
+
+def test_multiple_of_three():
+    assert process(3) == 'Fizz'
+    assert process(6) == 'Fizz'
+    assert process(9) == 'Fizz'
+
+
+def test_multiple_of_five_just_buzz():
+    assert process(5) == 'Buzz'
+    assert process(10) == 'Buzz'
+
+def test_multiple_of_15():
+    assert process(15) == "FizzBuzz"
+    assert process(30) == "FizzBuzz"
+
+def test_fizzbuzz_islist():
+    assert isinstance(get_fizzbuzz_list(),list)
+
+def test_fizzbuzz_list_has_len_100():
+    assert len(get_fizzbuzz_list()) == 100
+
+def test_fizzbuzz_list_first_15():
+    L = get_fizzbuzz_list()
+    assert L[:15] == [1,2,'Fizz',4,'Buzz','Fizz',7,8,'Fizz','Buzz',11,'Fizz',13,14,'FizzBuzz']
