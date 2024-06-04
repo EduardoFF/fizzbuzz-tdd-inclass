fc2e28d442cd6311286c7edc08be9cd47a7de374
“Eduardo <efeoflus@andrew.cmu.edu>
2024-06-04 11:49:42 +0300

    simple test for number one

---
+++ fizzbuzz.py
@@ -0,0 +1,6 @@
+
+def process(n):
+    return 1
+
+for number in range(1,101):
+    print(process(number))
---
+++ test_simple.py
@@ -0,0 +1,4 @@
+from fizzbuzz import process
+
+def test_number_one():
+    assert process(1) == 1
